b380b87f7fd21d710f78218460ba2a65da05dc60
Mikroklein <33251803+Mikroklein@users.noreply.github.com>
2020-10-15 16:31:52 +0300

    Delete myScript.js

--- js/myScript.js
+++
@@ -1,7 +0,0 @@
-let button=document.querySelector('.theme-button');
-let thumbsNumber=document.querySelector('.thumbs-number');
-let counter=1;
-
-button.onclick=function() {
-	thumbsNumber.textContent = counter;
-};
