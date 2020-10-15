3d6661293e47d565375b0ccc27ad25519dad0e95
Mikroklein <33251803+Mikroklein@users.noreply.github.com>
2020-10-15 16:31:43 +0300

    Delete likes.js

--- js/likes.js
+++
@@ -1,8 +0,0 @@
-let button=document.querySelector('.thumbsup');
-let thumbsNumber=document.querySelector('.thumbs-number');
-
-button.onclick=function() {
-	if (button.classList.contains('added')) {thumbsNumber.textContent--}
-	else {thumbsNumber.textContent++;}	
-	button.classList.toggle('added');
-};
